ea15ecfa32bfa1deafea2da597ac0e2f4726c0a5
anilconger <anilconger@gmail.com>
2024-02-19 23:26:46 +0300

    Updated BinaryGap.cpp File

--- src/BinaryGap.cpp
+++ src/BinaryGap.cpp
@@ -1,45 +1,46 @@
+
+
 #include <iostream>
 using namespace std;
+#include <vector>
 
 int  solution(int N) {
-	int value=0, value2=0, counter=0;
+	vector<int>binaryVector;
+	vector <int>OnesVector;
 	int copyOfN, copyofN2 = N;
-	int max=0;
-	
-	while (N!=0) {
+	int counter = 0, counterDiff = 0;
+	int max = 0;
+
+	while (N != 0) {
 		N = N / 2;
 		counter++;
 	}
-	int* pbinaryArray = new int[counter];
 	while (copyofN2 != 0) {
 		copyOfN = copyofN2 % 2;
 		copyofN2 = copyofN2 / 2;
-		pbinaryArray[value] = copyOfN;
-		value++;
+		binaryVector.push_back(copyOfN);
 	}
-	int* pOnesArray = new int[value2];
-	for (int i = (counter-1); i>=0; i--) {
-		if (pbinaryArray[i] == 1) {
-			pOnesArray[value2] = i;
-			value2++;
+	for (int i = binaryVector.size() - 1; i >= 0; i--) {
+		if (binaryVector[i] == 1)
+			OnesVector.push_back(i);
+	}
+	if (OnesVector.size() == 0 || OnesVector.size() == 1 || OnesVector.size() == binaryVector.size()) {
+		cout << "There is No Gap Length " << endl;
+		return 0;
+	}
+	else {
+		for (int j = 0; j < OnesVector.size(); j++) {
+			if (j + 1 != OnesVector.size())
+				counterDiff = (OnesVector[j] - OnesVector[j + 1] - 1);
+			if (counterDiff > max)
+				max = counterDiff;
 		}
-	}
-	if (value2 == 0 || value2 == 1) {
-		cout << "There is No Gap Length";
-	}
-	else{
-		for (int k = 0; k < value2; k++) {
-		if (max < (pOnesArray[k] - pOnesArray[k + 1]-1)) 
-			max = (pOnesArray[k] - pOnesArray[k + 1]-1);	
-	}
-	cout << "Gap Length: "<<max;
+		cout << max;
 	}
 	return max;
 }
 
-
 int main() {
-
-	solution(1041);
+	solution(59);
 	return 0;
 }
